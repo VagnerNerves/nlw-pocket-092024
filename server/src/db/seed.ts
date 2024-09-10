3da01fb612c54df25244aee15e765eaedeb4c683
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-10 13:35:20 -0300

    feat: Create seed.

---
+++ server/src/db/seed.ts
@@ -0,0 +1,47 @@
+import { client, db } from '.'
+import { goals, goalCompletions } from './schema'
+import dayjs from 'dayjs'
+
+async function seed() {
+  await db.delete(goalCompletions)
+  await db.delete(goals)
+
+  const result = await db
+    .insert(goals)
+    .values([
+      {
+        title: 'Acordar Cedo',
+        desiredWeeklyFrequency: 2,
+      },
+      {
+        title: 'Me exercitar',
+        desiredWeeklyFrequency: 3,
+      },
+      {
+        title: 'Meditar',
+        desiredWeeklyFrequency: 1,
+      },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    {
+      goalId: result[0].id,
+      createdAt: startOfWeek.toDate(),
+    },
+    {
+      goalId: result[1].id,
+      createdAt: startOfWeek.add(1, 'day').toDate(),
+    },
+  ])
+}
+
+seed()
+  .then(() => {
+    console.log('🌱 Database seeded successfully!')
+  })
+  .finally(() => {
+    client.end()
+  })
