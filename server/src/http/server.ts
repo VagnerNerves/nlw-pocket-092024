2344130e30ae5410e054824b5b50a549a35d5f20
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-10 11:45:49 -0300

    feat: Add Fastify and config, add biome e editorconfig for ajusted code.

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import fastify from 'fastify'
+
+const app = fastify()
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP Server running!')
+  })
