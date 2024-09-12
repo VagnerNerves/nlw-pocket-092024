747fec9029503cb75a044460c92dec271ec738bd
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-12 20:32:47 -0300

    feat: Ajusted screen fot get pengind goals.

--- mobile/src/components/PendingGoals.tsx
+++ mobile/src/components/PendingGoals.tsx
@@ -1,17 +1,45 @@
 import { View } from 'react-native'
 
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+
+import { getPendingGoals } from '@/http/get-pending-goals'
+import { createGoalCompletion } from '@/http/create-goal-completion'
+
 import { OutlineButton } from '@/components/ui/OutlineButton'
 
 export function PendingGoals() {
+  const queryClient = useQueryClient()
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['pending-goals'],
+    queryFn: getPendingGoals,
+  })
+
+  if (isLoading || !data) {
+    return null
+  }
+
+  async function handleCreateGoalCompletion(goalId: string) {
+    await createGoalCompletion({ goalId })
+
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+  }
+
   return (
     <View style={{ flexDirection: 'row', gap: 12, flexWrap: 'wrap' }}>
-      <OutlineButton buttonProps={{ disabled: false }} text="Meditar" />
-      <OutlineButton
-        buttonProps={{ disabled: false }}
-        text="Praticar exercício"
-      />
-      <OutlineButton buttonProps={{ disabled: false }} text="Nadar" />
-      <OutlineButton buttonProps={{ disabled: true }} text="Acordar cedo" />
+      {data.pendingGoals.map(goal => {
+        return (
+          <OutlineButton
+            key={goal.id}
+            buttonProps={{
+              disabled: goal.completionCount >= goal.desiredWeeklyFrequency,
+              onPress: () => handleCreateGoalCompletion(goal.id),
+            }}
+            text={goal.title}
+          />
+        )
+      })}
     </View>
   )
 }
