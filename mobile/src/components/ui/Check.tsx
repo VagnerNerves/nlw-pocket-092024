648414ebde38bd51c107ceb07dc3f8129f182656
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-12 16:39:37 -0300

    feat: Created component Check.

---
+++ mobile/src/components/ui/Check.tsx
@@ -0,0 +1,77 @@
+import { THEME } from '@/theme/theme'
+import { Circle, CircleCheck } from 'lucide-react-native'
+import { Text, View } from 'react-native'
+
+type CheckProps = {
+  isCheck: boolean
+  text: string
+  icon?:
+    | 'sleepy'
+    | 'smile'
+    | 'glasses'
+    | 'tongue-out'
+    | 'incredible'
+    | 'out-of-the-curve'
+}
+export function Check({ isCheck, text, icon }: CheckProps) {
+  return (
+    <View
+      style={{
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+
+        paddingHorizontal: 16,
+        paddingVertical: 10,
+
+        backgroundColor: isCheck ? THEME.COLORS.PINK[970] : THEME.COLORS.BLACK,
+
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: isCheck ? THEME.COLORS.PINK[500] : THEME.COLORS.ZINC[800],
+      }}
+    >
+      {isCheck ? (
+        <CircleCheck size={16} color={THEME.COLORS.PINK[400]} />
+      ) : (
+        <Circle size={16} color={THEME.COLORS.ZINC[600]} />
+      )}
+
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: THEME.FONTS.INTER_MEDIUM,
+            fontSize: THEME.FONTS.SIZE.SM,
+            color: THEME.COLORS.ZINC[300],
+          }}
+        >
+          {text}
+        </Text>
+      </View>
+
+      {icon ? (
+        <Text
+          style={{
+            fontFamily: THEME.FONTS.INTER_MEDIUM,
+            fontSize: THEME.FONTS.SIZE.LG,
+          }}
+        >
+          {icon === 'sleepy' && '🥱'}
+          {icon === 'smile' && '🙂'}
+          {icon === 'glasses' && '😎'}
+          {icon === 'tongue-out' && '😜'}
+          {icon === 'incredible' && '🤨'}
+          {icon === 'out-of-the-curve' && '🤯'}
+        </Text>
+      ) : (
+        <View style={{ width: 18 }} />
+      )}
+    </View>
+  )
+}
