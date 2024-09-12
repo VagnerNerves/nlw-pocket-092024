0c9aada97a95daee19739aa9c2c946a4a9060a58
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-12 12:49:38 -0300

    feat: Created theme.

---
+++ mobile/src/theme/theme.ts
@@ -0,0 +1,32 @@
+export const THEME = {
+  COLORS: {
+    ZINC: {
+      50: '#FAFAFA',
+      100: '#F4F4F5',
+      300: '#D4D4D8',
+      400: '#A1A1AA',
+      500: '#71717A',
+      600: '#52525B',
+      800: '#27272A',
+      900: '#18181B',
+      950: '#09090B',
+    },
+    PINK: {
+      400: '#F472B6',
+      500: '#EC4899',
+    },
+    VIOLET: {
+      50: '#F5F3FF',
+      500: '#8B5CF6',
+    },
+  },
+  FONTS: {
+    INTER_REGULAR: 'Inter_400Regular',
+    INTER_MEDIUM: 'Inter_500Medium',
+    INTER_SEMIBOLD: 'Inter_600SemiBold',
+    SIZE: {
+      SM: 14,
+      BASE: 16,
+    },
+  },
+} as const
