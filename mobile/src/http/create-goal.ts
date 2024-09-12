7cb9a40ebbae682839c7c81768f101fdf4f464a8
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-12 20:58:00 -0300

    feat: Create goal .

---
+++ mobile/src/http/create-goal.ts
@@ -0,0 +1,22 @@
+import { env } from '@/env'
+
+export interface CreateGoalRequest {
+  title: string
+  desiredWeeklyFrequency: number
+}
+
+export async function createGoal({
+  title,
+  desiredWeeklyFrequency,
+}: CreateGoalRequest): Promise<void> {
+  await fetch(`${env.EXPO_PUBLIC_API_URL}/goals`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title,
+      desiredWeeklyFrequency,
+    }),
+  })
+}
