a775ad0812ceeac26b965a2feabdc1d062797c84
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-12 19:08:58 -0300

    feat: Ajusted for get summary.

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,11 +1,27 @@
 import { ScrollView } from 'react-native'
 
+import { useQuery } from '@tanstack/react-query'
+
+import { getSummary } from '@/http/get-summary'
+
 import { EmptyGoals } from '@/components/EmptyGoals'
+import { Summary } from '@/components/Summary'
+import { Loading } from '@/components/ui/Loading'
 
 export default function Home() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, //60 seconds
+  })
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-      <EmptyGoals />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
     </ScrollView>
   )
 }
