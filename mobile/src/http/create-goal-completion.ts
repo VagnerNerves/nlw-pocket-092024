164dfdd3f7026d577c7d48791f03e936dfa72836
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-12 20:32:26 -0300

    feat: Create goal competintion.

---
+++ mobile/src/http/create-goal-completion.ts
@@ -0,0 +1,23 @@
+import { env } from '@/env'
+
+export interface CreateGoalCompletionRequest {
+  goalId: string
+}
+
+export async function createGoalCompletion({
+  goalId,
+}: CreateGoalCompletionRequest): Promise<void> {
+  const response = await fetch(`${env.EXPO_PUBLIC_API_URL}/completions`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      goalId,
+    }),
+  })
+
+  if (!response.ok) {
+    throw new Error('Error while creating the goal completion')
+  }
+}
