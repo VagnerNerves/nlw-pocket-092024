6365ed00a078408ad3b3e475b800e67bc0f04506
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-13 08:39:17 -0300

    feat: Create delete goal Completion.

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -10,6 +10,7 @@ import { createGoalRoute } from './routes/create-goal'
 import { createCompletionRoute } from './routes/create-completion'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
+import { deleteCompletion } from './routes/delete-completion'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -24,6 +25,7 @@ app.register(createGoalRoute)
 app.register(createCompletionRoute)
 app.register(getPendingGoalsRoute)
 app.register(getWeekSummaryRoute)
+app.register(deleteCompletion)
 
 app
   .listen({
