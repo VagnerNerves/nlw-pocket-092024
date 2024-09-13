abdc746f4b9b638644e30767c18bc36fb9e63404
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-13 09:57:32 -0300

    feat: Add delete goal.

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 import dayjs from 'dayjs'
 import ptBR from 'dayjs/locale/pt-BR'
@@ -6,6 +6,8 @@ import ptBR from 'dayjs/locale/pt-BR'
 import { CheckCircle2, Plus } from 'lucide-react'
 
 import { getSummary } from '../http/get-summary'
+import { deleteGoalCompletion } from '../http/delete-goal-completion'
+
 import { PendingGoals } from './pending-goals'
 
 import { InOrbitIcon } from './in-orbit-icon'
@@ -18,6 +20,8 @@ import { Separator } from './ui/separator'
 dayjs.locale(ptBR)
 
 export function Summary() {
+  const queryClient = useQueryClient()
+
   const { data } = useQuery({
     queryKey: ['summary'],
     queryFn: getSummary,
@@ -32,6 +36,13 @@ export function Summary() {
   const lastDayOfWeek = dayjs().endOf('week').format('D MMM')
 
   const completePercentage = Math.round((data?.completed * 100) / data?.total)
+
+  async function handleDeleteCompletion(id: string) {
+    await deleteGoalCompletion({ id })
+
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+  }
 
   return (
     <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
@@ -74,36 +85,54 @@ export function Summary() {
       <div className="flex flex-col gap-6">
         <h2 className="text-xl font-medium">Sua semana</h2>
 
-        {Object.entries(data.goalsPerDay).map(([date, goals]) => {
-          const weekDAy = dayjs(date).format('dddd')
-          const formattedDate = dayjs(date).format('D[ de ]MMMM')
+        {!data.goalsPerDay && (
+          <p className="font-normal text-sm text-zinc-400">
+            Você ainda não completou nenhuma meta essa semana.
+          </p>
+        )}
 
-          return (
-            <div key={date} className="flex flex-col gap-4">
-              <h3 className="font-medium">
-                <span className="capitalize">{weekDAy}</span>{' '}
-                <span className="text-zinc-400 text-xs">({formattedDate})</span>
-              </h3>
+        {data.goalsPerDay &&
+          Object.entries(data.goalsPerDay).map(([date, goals]) => {
+            const weekDAy = dayjs(date).format('dddd')
+            const formattedDate = dayjs(date).format('D[ de ]MMMM')
 
-              <ul className="flex flex-col gap-3">
-                {goals.map(goal => {
-                  const parsedTime = dayjs(goal.completedAt).format('HH:mm[h]')
+            return (
+              <div key={date} className="flex flex-col gap-4">
+                <h3 className="font-medium">
+                  <span className="capitalize">{weekDAy}</span>{' '}
+                  <span className="text-zinc-400 text-xs">
+                    ({formattedDate})
+                  </span>
+                </h3>
 
-                  return (
-                    <li key={goal.id} className="flex items-center gap-2">
-                      <CheckCircle2 className="size-4 text-pink-500" />
-                      <span className="text-sm text-zinc-400">
-                        Você completou "
-                        <span className="text-zinc-100">{goal.title}</span>" às{' '}
-                        <span className="text-zinc-100">{parsedTime}</span>
-                      </span>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-          )
-        })}
+                <ul className="flex flex-col gap-3">
+                  {goals.map(goal => {
+                    const parsedTime = dayjs(goal.completedAt).format(
+                      'HH:mm[h]'
+                    )
+
+                    return (
+                      <li key={goal.id} className="flex items-center gap-2">
+                        <CheckCircle2 className="size-4 text-pink-500" />
+                        <span className="text-sm text-zinc-400">
+                          Você completou "
+                          <span className="text-zinc-100">{goal.title}</span>"
+                          às <span className="text-zinc-100">{parsedTime}</span>
+                        </span>
+                        <button
+                          type="button"
+                          className="text-xs text-zinc-500 underline"
+                          onClick={() => handleDeleteCompletion(goal.id)}
+                        >
+                          Desfazer
+                        </button>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+            )
+          })}
       </div>
     </div>
   )
