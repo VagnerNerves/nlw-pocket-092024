b2273210c3b26985e3b7120f0258f460fbac02e3
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-13 09:56:57 -0300

    feat: Created delete goal.

---
+++ web/src/http/delete-goal-completion.ts
@@ -0,0 +1,17 @@
+import { env } from '../env'
+
+export interface DeleteGoalCompletionRequest {
+  id: string
+}
+
+export async function deleteGoalCompletion({
+  id,
+}: DeleteGoalCompletionRequest): Promise<void> {
+  const response = await fetch(`${env.VITE_API_URL}/completions/${id}`, {
+    method: 'DELETE',
+  })
+
+  if (!response.ok) {
+    throw new Error('Error while delete the goal completion')
+  }
+}
