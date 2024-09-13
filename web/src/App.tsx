2c54ee75230e5af6c610d4966d9af484bda076cc
Vagner Nerves <vagnernervessantos@gmail.com>
2024-09-13 10:21:15 -0300

    feat: Add Loading on home.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,13 +6,18 @@ import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
 import { EmptyGoals } from './components/empty-goals'
+import { Loading } from './components/ui/loading'
 
 export function App() {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['summary'],
     queryFn: getSummary,
     staleTime: 1000 * 60, //60 seconds
   })
+
+  if (isLoading) {
+    return <Loading divProps={{ className: 'h-screen' }} />
+  }
 
   return (
     <Dialog>
